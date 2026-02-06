f4579eef5d5cdd53a7110b021a4cd285bf79d2fb
Zolboobayar Gantumur <hello@zolbooo.xyz>
2026-02-06 18:57:47 +0900

    fix(devcontainer): install missing dependencies for tailwindcss

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,7 +5,7 @@ uv tool install invoke
 uv tool install ruff
 
 uv sync --group local
-npm install -g tailwindcss
+npm install -g tailwindcss@3 flowbite @tailwindcss/{typography,forms,aspect-ratio,container-queries}
 
 echo "You are good to go! 🎉"
 echo "You can run the following command to start the development server:"
